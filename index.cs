a22606f649303ac79582ca598203bd3f342e69ba
cucarrox <brunocucarrox@gmail.com>
2023-10-18 21:22:40 -0300

    Terceira modificação

--- index.cs
+++ index.cs
@@ -11,6 +11,8 @@ namespace Program
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
